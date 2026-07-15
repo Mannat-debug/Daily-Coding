0865c5920415e72e6d3c7ffdc53943195b7033b7
Mannat-debug <ms.mahi0906@gmail.com>
2026-07-15 11:26:57 +0530

    calculator

---
+++ learningcpp/calculator.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+// take input from the user and store it in a variable
+// take operation from the user and store it in a variable
+// take second input from the user and store it in a variable
+// calculate result
+// print result
+int m
