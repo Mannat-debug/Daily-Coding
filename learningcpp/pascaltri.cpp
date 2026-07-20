e4314495a174916be8463fca19bb1583642e2668
Mannat-debug <ms.mahi0906@gmail.com>
2026-07-20 13:07:11 +0530

    day 6

---
+++ learningcpp/pascaltri.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        int value = 1;
+
+        for (int j = 0; j <= i; j++) {
+            cout << value << " ";
+
+            // Calculate the next value in the row
+            value = value * (i - j) / (j + 1);
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
